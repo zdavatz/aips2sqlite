94d5c88568c9457ee15cd5e08ee97e5b8ba2f57a
zdavatz@gmail.com <zdavatz@gmail.com>
2022-02-09 08:38:39 +0100

    Fix for #33

--- src/com/maxl/java/aips2sqlite/AllDown.java
+++ src/com/maxl/java/aips2sqlite/AllDown.java
@@ -277,9 +277,9 @@ public class AllDown {
 			SOAPBody soapBody = envelope.getBody();
 			// Construct SOAP request message
 			SOAPElement soapBodyElement1 = soapBody.addChildElement("DownloadArticleInput");
-			soapBodyElement1.addNamespaceDeclaration("", "http://refdatabase.refdata.ch/");				
+			soapBodyElement1.addNamespaceDeclaration("", "https://refdatabase.refdata.ch/");				
 			SOAPElement soapBodyElement2 = soapBodyElement1.addChildElement("ATYPE");
-			soapBodyElement2.addNamespaceDeclaration("", "http://refdatabase.refdata.ch/Article_in");			
+			soapBodyElement2.addNamespaceDeclaration("", "https://refdatabase.refdata.ch/Article_in");			
 			soapBodyElement2.addTextNode("ALL");	
 			soapRequest.saveChanges();
 			// If needed print out soapRequest in a pretty format
@@ -288,7 +288,7 @@ public class AllDown {
 			SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();			
 			SOAPConnection connection = soapConnectionFactory.createConnection();			
 			// wsURL contains service end point
-			String wsURL = "http://refdatabase.refdata.ch/Service/Article.asmx?WSDL";
+			String wsURL = "https://refdatabase.refdata.ch/Service/Article.asmx?WSDL";
 			SOAPMessage soapResponse = connection.call(soapRequest, wsURL);
 			// Extract response
 			Document doc = soapResponse.getSOAPBody().extractContentAsDocument();
@@ -339,9 +339,9 @@ public class AllDown {
 			SOAPBody soapBody = envelope.getBody();
 			// Construct SOAP request message
 			SOAPElement soapBodyElement1 = soapBody.addChildElement("DownloadPartnerInput");
-			soapBodyElement1.addNamespaceDeclaration("", "http://refdatabase.refdata.ch/");				
+			soapBodyElement1.addNamespaceDeclaration("", "https://refdatabase.refdata.ch/");				
 			SOAPElement soapBodyElement2 = soapBodyElement1.addChildElement("PTYPE");
-			soapBodyElement2.addNamespaceDeclaration("", "http://refdatabase.refdata.ch/Partner_in");			
+			soapBodyElement2.addNamespaceDeclaration("", "https://refdatabase.refdata.ch/Partner_in");			
 			soapBodyElement2.addTextNode("ALL");	
 			soapRequest.saveChanges();
 			// If needed print out soapRequest in a pretty format
@@ -350,7 +350,7 @@ public class AllDown {
 			SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();			
 			SOAPConnection connection = soapConnectionFactory.createConnection();			
 			// wsURL contains service end point
-			String wsURL = "http://refdatabase.refdata.ch/Service/Partner.asmx?WSDL";
+			String wsURL = "https://refdatabase.refdata.ch/Service/Partner.asmx?WSDL";
 			SOAPMessage soapResponse = connection.call(soapRequest, wsURL);
 			// Extract response
 			Document doc = soapResponse.getSOAPBody().extractContentAsDocument();
