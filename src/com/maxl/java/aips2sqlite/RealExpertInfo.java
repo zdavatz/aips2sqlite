5f7d18fbf2e0334ca2f43a4767be625c0638d293
b123400 <i@b123400.net>
2024-03-15 12:04:37 +0900

    Handle case of empty flagSB20

--- src/com/maxl/java/aips2sqlite/RealExpertInfo.java
+++ src/com/maxl/java/aips2sqlite/RealExpertInfo.java
@@ -1344,7 +1344,7 @@ public class RealExpertInfo {
 							if (add_info_str != null) {
 								List<String> ai_list = Arrays.asList(add_info_str.split("\\s*;\\s*"));
 								if (ai_list != null) {
-									if (!ai_list.get(0).isEmpty())
+									if (ai_list.size() >= 1 && !ai_list.get(0).isEmpty())
 										orggen_str = ai_list.get(0);
 								}
 							}
@@ -1543,9 +1543,9 @@ public class RealExpertInfo {
 			if (addinfo_str != null) {
 				List<String> ai_list = Arrays.asList(addinfo_str.split("\\s*;\\s*"));
 				if (ai_list != null) {
-					if (!ai_list.get(0).isEmpty())
+					if (ai_list.size() >= 1 && !ai_list.get(0).isEmpty())
 						orggen_str = ", " + ai_list.get(0);		// O + G
-					if (!ai_list.get(1).isEmpty())
+					if (ai_list.size() >= 2 && !ai_list.get(1).isEmpty())
 						flagsb_str = ", " + ai_list.get(1);		// SB
 				}
 			}
